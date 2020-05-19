f113e603b8e6348c98ff4140df36c15ba5b14d44
PeteTheN00b <ropeter63@googlemail.com>
2020-05-19 23:43:00 +0200

    Dear god I've finally drawn a circle

--- source/example.cpp
+++ source/example.cpp
@@ -3,15 +3,25 @@
 #include <utility>
 #include <cmath>
 
+#include "classes/circle.hpp"
+
 
 int main(int argc, char* argv[])
 {
   Window win{std::make_pair(800,800)};
 
+  myShapes::Circle c1{ {300, 300}, {100}, {255, 128, 0} };
+  myShapes::Circle c2{ {500, 350}, {200}, {50, 150, 0} };
+  myShapes::Circle c3{ {350, 600}, {75}, {80, 0, 255} };
+
   while (!win.should_close()) {
     if (win.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
       win.close();
     }
+
+    c1.draw(&win);
+    c2.draw(&win);
+    c3.draw(&win, 50);
 
     bool left_pressed = win.get_mouse_button(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
 
